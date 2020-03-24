b14a3eab2345b4441b745bb99db6fc6a7cfbf9f6
Sindhu <sindhujav18@gmail.com>
2020-03-24 16:27:33 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # node-server
+## What is Node.js?
+- Node.js is an open source server environment
+- Node.js is free
+- Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+- Node.js uses JavaScript on the server
+
+## Reference
+
+- [w3schools](https://www.w3schools.com/nodejs/nodejs_intro.asp)
---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '192.168.0.2';
+const port = 3002;
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Sindhuja Valeti - Client-Side + Server Side=Full Stack Development');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
